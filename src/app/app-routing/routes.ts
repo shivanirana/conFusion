8b7712b1313086f5d69514a9dbc4ac42f4a3079f
Shivani <shivanirana24@gmail.com>
2020-03-29 17:30:39 +0900

    Single Page Applications Part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -8,6 +8,7 @@ import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
 { path:'home', component: HomeComponent },
-{ path: 'menu', component: MenuComponent },
+{ path: 'menu', component: MenuComponent }, 
+{ path: 'contactus',     component: ContactComponent },
 { path: '',redirectTo:'/home',pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule} from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
